ad08c741c786647c2ee8e17c090792b291aaa0e1
davinaa018 <davinaa018@gmail.com>
2023-04-12 20:32:09 -0600

    feature: Added WebService, BoardPanel, and JSONObject package

---
+++ WebService.java
@@ -0,0 +1,113 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONObject;
+
+
+public class WebService {
+    private String url;
+    private URL conn;
+    private HttpURLConnection con;
+    private BufferedReader in;
+    private StringBuffer response;
+    private JSONObject obj;
+
+    public WebService(String url){
+        this.url = url;
+    }
+
+    public void getInfo(){
+        try{
+            // Create a URL Object for web service
+            conn = new URL(url+"info/");
+            // Open a connection to the web service
+            con = (HttpURLConnection) conn.openConnection();
+
+            // Set the request method to GET
+            con.setRequestMethod("GET");
+
+            // Read the response from the web service
+            in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // Convert the response to a JSON object
+            obj = new JSONObject(response.toString());
+            System.out.println(obj.toString());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public String getNew(String strategy){
+        try{
+            // Create a URL Object for web service
+            conn = new URL(url+"new/?strategy="+strategy);
+            // Open a connection to the web service
+            con = (HttpURLConnection) conn.openConnection();
+
+            // Set the request method to GET
+            con.setRequestMethod("GET");
+
+            // Read the response from the web service
+            in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // Convert the response to a JSON object
+            obj = new JSONObject(response.toString());
+            System.out.println(obj.toString());
+            String pid = obj.get("pid").toString();
+            return pid;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void getPlay(String pid, int x, int y){
+        try{
+            // Create a URL Object for web service
+            conn = new URL(url+"play/?pid="+ pid + "&x=" + x + "&y=" + y);
+            // Open a connection to the web service
+            con = (HttpURLConnection) conn.openConnection();
+
+            // Set the request method to GET
+            con.setRequestMethod("GET");
+
+            // Read the response from the web service
+            in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // Convert the response to a JSON object
+            obj = new JSONObject(response.toString());
+            System.out.println(obj.toString());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args){
+        String url = "http://omok.atwebpages.com/";
+        WebService ws = new WebService(url);
+        ws.getInfo();
+        String pid = ws.getNew("Random");
+        ws.getPlay(pid, 1, 1);
+    }
+}
---
+++ view/BoardPanel.java
@@ -0,0 +1,167 @@
+package view;
+
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import lib.Board;
+import lib.Coordinate;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+public class BoardPanel extends JPanel {
+    private static final Color boardColor = new Color(253, 218, 13);
+    private Coordinate hoverIntersection;
+    private Board board;
+
+    public BoardPanel(Board board){
+        this.board = board;
+        setBackground(boardColor);
+
+        // Hovering effect
+        hoveringEffect(false);
+        // Place stones
+        placeStone(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        int size = board.getSize();
+        int squareSize = getWidth()/size;
+        drawBoard(size, squareSize, g);
+        drawHoverEffect(size, squareSize, g);
+        drawStones(size, squareSize, g);
+    }
+
+    /**
+     * Draws the board
+     * @param size
+     * @param squareSize
+     * @param g
+     */
+    private void drawBoard(int size, int squareSize, Graphics g){
+        // Draw horizontal lines
+        for (int i = 0; i < size; i++) {
+           g.drawLine(0, i * squareSize, getWidth(), i * squareSize );
+       }
+       // Draw vertical lines
+       for (int i = 0; i < size; i++) {
+           g.drawLine(i * squareSize, 0, i * squareSize, getHeight());
+       }
+   }
+
+   /**
+     * Draws the hovering effect
+     * @param size
+     * @param squareSize
+     * @param g
+     */
+    private void drawHoverEffect(int size, int squareSize, Graphics g){
+        if (hoverIntersection != null) {
+            int x = hoverIntersection.getX() * squareSize + squareSize-7;
+            int y = hoverIntersection.getY() * squareSize + squareSize-7;
+            int ovalSize = squareSize / 2;
+            g.drawOval(x, y, ovalSize, ovalSize);
+        }
+    }
+
+    /**
+     * Draws the stones
+     * @param size
+     * @param squareSize
+     * @param g
+     */
+    private void drawStones(int size, int squareSize, Graphics g){
+        //  Draw Stones
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int x = i * squareSize + squareSize / 2;
+                int y = j * squareSize + squareSize / 2;
+                if (board.getPieceAt(i, j) == 'X') {
+                    g.setColor(Color.BLACK);
+                    g.fillOval(x+8, y+8, squareSize / 2, squareSize / 2);
+                    if (board.hasWinner(i, j)) {
+                        g.setColor(Color.RED);
+                        g.fillOval(x+10, y+10, squareSize / 2 - 4, squareSize / 2 - 4);
+                    }
+                } else if (board.getPieceAt(i, j) == 'O') {
+                    g.setColor(Color.WHITE);
+                    g.fillOval(x+8, y+8, squareSize / 2, squareSize / 2);
+                    g.setColor(Color.BLACK);
+                    if (board.hasWinner(i, j)) {
+                        g.setColor(Color.RED);
+                        g.fillOval(x+10, y+10, squareSize / 2 - 4, squareSize / 2 - 4);
+                    }
+                }
+            }
+        }
+    }
+
+     /**
+     * Adds a hovering effect to the board
+     * @param startGame
+     */
+    public void hoveringEffect(boolean startGame){
+        if(startGame){
+            // Hovering effect
+            addMouseMotionListener(new MouseMotionAdapter() {
+                @Override
+                public void mouseMoved(MouseEvent e) {
+                    int size = board.getSize();
+                    int squareSize = getWidth() / size;
+                    int i = e.getX() / squareSize;
+                    int j = e.getY() / squareSize;
+                    if (i >= 0 && i < size && j >= 0 && j < size) {
+                        hoverIntersection = new Coordinate(i, j);
+                    } else {
+                        hoverIntersection = null;
+                    }
+                    repaint();
+                }
+            });
+        }
+    }
+
+     /**
+     * Places a stone on the board by using MouseListener
+     * @param startGame
+     */
+    public void placeStone(boolean startGame) {
+        if(startGame){
+            // Place stones on the board
+            addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        int i = e.getX() / (getWidth() / board.getSize());
+                        int j = e.getY() / (getWidth() / board.getSize());
+                        if (i >= 0 && i < board.getSize() && j >= 0 && j < board.getSize()) {
+                            if (board.getPieceAt(i, j) == '-') {
+                                // place stone and switch player
+                                board.makeMove(i, j, 'X');
+                                System.out.println("Player 1 placed a stone at " + i + ", " + j);
+                                repaint();
+
+                            }
+                        }
+                    }
+                });
+        }
+    }
+
+    public static void main(String[] args) {
+        Board board = new Board(15);
+        BoardPanel panel = new BoardPanel(board);
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+
+    }
+    
+}
